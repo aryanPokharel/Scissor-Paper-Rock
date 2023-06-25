433620f902ca048fba34176abfe1cf697904aca4
Aryan Pokharel <64863058+aryanPokharel@users.noreply.github.com>
2023-06-25 13:03:02 +0545

    home Bg added

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -13,84 +13,86 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Paper Scissor Rock"),
-                FractionallySizedBox(
-                  widthFactor: 0.9,
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: (() => {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SinglePlayer()),
-                            )
-                          }),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue),
-                      child: const Text("Single Player"),
-                    ),
-                  ),
-                ),
-                FractionallySizedBox(
-                  widthFactor: 0.9,
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: (() => {print("Multiplayer")}),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.amber),
-                      child: const Text("Multiplayer"),
-                    ),
-                  ),
-                ),
-                FractionallySizedBox(
-                  widthFactor: 0.9,
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: (() => {print("Multiplayer")}),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green),
-                      child: const Text("Online"),
-                    ),
-                  ),
-                ),
-                FractionallySizedBox(
-                  widthFactor: 0.9,
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: (() => {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const LeaderBoards()),
-                            )
-                          }),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.cyan),
-                      child: const Text("Leaderboards"),
-                    ),
-                  ),
-                ),
-                FractionallySizedBox(
-                  widthFactor: 0.9,
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: (() => {print("Multiplayer")}),
-                      style:
-                          ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                      child: const Text("Settings"),
-                    ),
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/homeBg.png'),
+                repeat: ImageRepeat.repeat,
+              ),
             ),
           ),
-        ),
+          SafeArea(
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FractionallySizedBox(
+                      widthFactor: 0.9,
+                      child: SizedBox(
+                        child: ElevatedButton(
+                          onPressed: (() => {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SinglePlayer()),
+                                )
+                              }),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue),
+                          child: const Text("Single Player"),
+                        ),
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      widthFactor: 0.9,
+                      child: SizedBox(
+                        child: ElevatedButton(
+                          onPressed: (() => {print("Multiplayer")}),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.amber),
+                          child: const Text("Multiplayer"),
+                        ),
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      widthFactor: 0.9,
+                      child: SizedBox(
+                        child: ElevatedButton(
+                          onPressed: (() => {print("Multiplayer")}),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green),
+                          child: const Text("Online"),
+                        ),
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      widthFactor: 0.9,
+                      child: SizedBox(
+                        child: ElevatedButton(
+                          onPressed: (() => {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const LeaderBoards()),
+                                )
+                              }),
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.cyan),
+                          child: const Text("Leaderboards"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     ));
   }
